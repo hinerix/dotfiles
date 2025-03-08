4da417260729edd457294b22090bbc08b7bf2160
hinerix <197036486+hinerix@users.noreply.github.com>
2025-03-08 23:42:42 +0900

    fishの設定ファイルを追加

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -U FZF_LEGACY_KEYBINDINGS 0
+    set -g theme_display_git_default_branch yes
+
+		#abbr
+		abbr ls ls -alSh
+		abbr vi nvim
+
+end
