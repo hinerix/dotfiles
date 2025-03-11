5d0bc131482c2efe55200a2848a40ee775e27b2a
hinericubix <197036486+hinericubix@users.noreply.github.com>
2025-03-11 18:25:03 +0900

    lspとcmpの連携

--- .config/nvim/lua/rc/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/rc/plugins/lsp/lspconfig.lua
@@ -9,6 +9,35 @@ return {
 		local lspconfig = require("lspconfig")
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 		local mason_lspconfig = require("mason-lspconfig")
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function()
+			end
+		})
 
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		mason_lspconfig.setup_handlers({
+			function(server_name)
+				lspconfig[server_name].setup({
+					capabilities = capabilities,
+				})
+			end,
+			["lua_ls"] = function()
+				lspconfig["lua_ls"].setup({
+					capabilities = capabilities,
+					settings = {
+						Lua = {
+							diagnostics = {
+								globals = { "vim" },
+							},
+							comletion = {
+								callSnippet = "Replace",
+							},
+						},
+					},
+				})
+			end,
+		})
 	end
 }
