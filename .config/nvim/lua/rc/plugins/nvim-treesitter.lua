93d8b9dd277e0dd26594c3bb1e8491c8b99b98f2
hinerix <197036486+hinerix@users.noreply.github.com>
2025-03-18 09:53:58 +0900

    nvim-ts-autotagの設定の調整

--- .config/nvim/lua/rc/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/rc/plugins/nvim-treesitter.lua
@@ -1,19 +1,13 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	main = "nvim-treesitter.configs", -- optsで使用するモジュールを定義
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
-	event = { "BufReadPre", "BufNewFile" },
 	opts = {
 		highlight = {
 			enable = true,
 		},
 		indent = {
-			enable = true,
-		},
-		autotag = {
 			enable = true,
 		},
 		ensure_installed = {
