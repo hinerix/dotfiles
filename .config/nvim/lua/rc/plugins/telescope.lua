967af8043b659fc28ad43ba60394c1cd60da3f27
hinerix <197036486+hinerix@users.noreply.github.com>
2025-03-18 16:21:03 +0900

    Telescopeの追加

---
+++ .config/nvim/lua/rc/plugins/telescope.lua
@@ -0,0 +1,27 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
+
+		telescope.setup({
+			defaults = {
+			path_display = { "smart" },
+			}
+		})
+
+		vim.keymap.set("n", "<Leader>ff", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<Leader>fr", builtin.oldfiles, { desc = "Recent files" })
+    vim.keymap.set("n", "<Leader>fk", builtin.keymaps, { desc = "KeyMaps" })
+    vim.keymap.set("n", "<Leader>fh", builtin.help_tags, { desc = "Find help tags" })
+    vim.keymap.set("n", "<Leader>fc", builtin.command_history, { desc = "Command histories" })
+    vim.keymap.set("n", "<Leader>fb", builtin.buffers, { desc = "Find buffers" })
+    vim.keymap.set("n", "<Leader>fg", builtin.live_grep, { desc = "Live grep" })
+	end
+}
