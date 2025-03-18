c7c98d75ecf916c6e21709d3338bfae53fbc750b
hinerix <197036486+hinerix@users.noreply.github.com>
2025-03-18 13:04:39 +0900

    lspの診断情報のアイコンを表示

--- .config/nvim/lua/rc/plugins/bufferline.lua
+++ .config/nvim/lua/rc/plugins/bufferline.lua
@@ -5,7 +5,21 @@ return {
 	opts = {
 		options = {
 			separator_style = "slant",
-			show_buffer_close_icons = false,
+			-- show_buffer_close_icons = false,
+			buffer_close_icon = '✕ ',
+			indicator = {
+				style = "underline",
+			},
+			diagnostics = "nvim_lsp",
+			diagnostics_indicator = function(count, level)
+        local icon = level:match("error") and " " or " "
+        return " " .. icon .. count
+			end,
+			hover = {
+				enabled = true,
+				delay = 0,
+				reveal = { "close" },
+			},
 		},
 	},
 }
