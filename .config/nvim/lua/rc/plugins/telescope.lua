45785dc343762de74b1309c49e87df6c8a7a1a7a
hinerix <197036486+hinerix@users.noreply.github.com>
2025-03-23 11:37:54 +0900

    diagnostics確認用のキーマップ追加

--- .config/nvim/lua/rc/plugins/telescope.lua
+++ .config/nvim/lua/rc/plugins/telescope.lua
@@ -19,9 +19,10 @@ return {
 		vim.keymap.set("n", "<Leader>ff", builtin.find_files, { desc = "Find files" })
 		vim.keymap.set("n", "<Leader>fr", builtin.oldfiles, { desc = "Recent files" })
     vim.keymap.set("n", "<Leader>fk", builtin.keymaps, { desc = "KeyMaps" })
-    vim.keymap.set("n", "<Leader>fh", builtin.help_tags, { desc = "Find help tags" })
+    vim.keymap.set("n", "<Leader>fh", builtin.help_tags, { desc = "Help tags" })
     vim.keymap.set("n", "<Leader>fc", builtin.command_history, { desc = "Command histories" })
-    vim.keymap.set("n", "<Leader>fb", builtin.buffers, { desc = "Find buffers" })
+    vim.keymap.set("n", "<Leader>fb", builtin.buffers, { desc = "Buffers" })
     vim.keymap.set("n", "<Leader>fg", builtin.live_grep, { desc = "Live grep" })
+    vim.keymap.set("n", "<Leader>fd", builtin.diagnostics, { desc = "Diagnostics" })
 	end
 }
