424d44e159cc4b8758af17f2f3d81bc31baec515
hinerix <197036486+hinerix@users.noreply.github.com>
2025-04-02 00:57:19 +0900

    render-markdown.nvimの設定を調整

--- .config/nvim/lua/rc/plugins/render-markdown.lua
+++ .config/nvim/lua/rc/plugins/render-markdown.lua
@@ -1,10 +1,10 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	ft = { "markdown", "quarto" },
+	ft = { "markdown" },
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	opts = {
 		render_modes = true,
 		completions = { lsp = { enabled = true } },
 		sign = { enabled = false },
-	}
+	},
 }
