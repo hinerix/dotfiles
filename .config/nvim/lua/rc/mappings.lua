bd7a64f33867245882d638090e974b7c0edd5a4c
hinericubix <197036486+hinericubix@users.noreply.github.com>
2025-04-02 21:51:42 +0900

    Tabの機能は使ってないのでキーバインドを削除

--- .config/nvim/lua/rc/mappings.lua
+++ .config/nvim/lua/rc/mappings.lua
@@ -9,16 +9,6 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", opts("Clean highlight"))
 vim.keymap.set({ "n", "v" }, "x", '"_x', { noremap = true, silent = true })
 
 ----------------------------
--- Tab
-----------------------------
-vim.keymap.set("n", "<Leader>tc", "<Cmd>tabnew<CR>", opts("Open new tab"))
-vim.keymap.set("n", "<Leader>tq", "<Cmd>tabclose<CR>", opts("Close current tab"))
-vim.keymap.set("n", "[t", ":tabprevious<CR>",opts("Go to previous tab"))
-vim.keymap.set("n", "]t", ":tabnext<CR>", opts("Go to next tab"))
-vim.keymap.set("n", "[T", ":tabfirst<CR>", opts("Go to first tab"))
-vim.keymap.set("n", "]T", ":tablast<CR>", opts("Go to last tab"))
-
-----------------------------
 -- Buffers
 ----------------------------
 im.keymap.set("n", "<Leader>bd", "<Cmd>bdelete<CR>", opts("Delete current buffer"))
