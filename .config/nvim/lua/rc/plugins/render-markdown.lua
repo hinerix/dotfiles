6843b1a4a4c101b7223dc888729e63bcf0bb2798
hinericubix <197036486+hinericubix@users.noreply.github.com>
2025-04-03 17:17:56 +0900

    headingやcolorsを調整

--- .config/nvim/lua/rc/plugins/render-markdown.lua
+++ .config/nvim/lua/rc/plugins/render-markdown.lua
@@ -2,18 +2,43 @@ return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	ft = { "markdown" },
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	opts = {
-		render_modes = true,
-		completions = { lsp = { enabled = true } },
-		sign = { enabled = false },
-		heading = {
-			position = "inline",
-			backgrounds = {},
-		},
-		code = {
-			width = "block",
-			left_pad = 2,
-			right_pad = 4,
-		},
-	},
+	config = function()
+
+		local render_markdown = require("render-markdown")
+
+		render_markdown.setup({
+			completions = { blink = { enabled = true } },
+			sign = { enabled = false },
+			latex = { enabled = false },
+			heading = {
+				render_modes = true,
+				icons = { "󰬺  ", "󰬻  ", "󰬼  ", "󰬽  ", "󰬾  ", "󰬿  " },
+				position = "inline",
+				backgrounds = {},
+			},
+			checkbox = {
+				unchecked = { icon = "󰄱 " },
+				checked = { icon = "󰄵 " },
+				custom = { todo = { rendered = " " } },
+			},
+			pipe_table = { preset = "round" },
+			link = {
+				custom = {
+					python = { pattern = "%.py$", icon = "󰌠 " },
+					markdown = { pattern = "%.md$", icon = "󰍔 " },
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>mr", function()
+			render_markdown.toggle()
+		end, { desc = "[M]arkdown [R]ender toggle" })
+		local C = require("catppuccin.palettes").get_palette()
+		vim.api.nvim_set_hl(0, "@markup.quote", { fg = C.text, bold = false })
+		vim.api.nvim_set_hl(0, "@markup.link.label", { fg = C.lavender, bold = false })
+		vim.api.nvim_set_hl(0, "@markup.link.url", { fg = C.lavender, bold = false })
+		vim.api.nvim_set_hl(0, "RenderMarkdownLink", { fg = C.lavender, bold = false })
+		vim.api.nvim_set_hl(0, "RenderMarkdownWikiLink", { fg = C.lavender, bold = false })
+
+	end
 }
