4ceb350216da02fc37ad0fdcc10142f69e31ceac
hinerix <197036486+hinerix@users.noreply.github.com>
2025-05-07 23:47:05 +0900

    nvim-lspconfigからの脱却の設定のため、一旦コメントアウト

--- .config/nvim/lua/lsp/mason.lua
+++ .config/nvim/lua/lsp/mason.lua
@@ -1,13 +1,13 @@
-return {
-	"williamboman/mason.nvim",
-	build = ":MasonUpdate",
-	opts = {
-		ui = {
-			icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗",
-			},
-		},
-	},
-}
+-- return {
+-- 	"williamboman/mason.nvim",
+-- 	build = ":MasonUpdate",
+-- 	opts = {
+-- 		ui = {
+-- 			icons = {
+-- 				package_installed = "✓",
+-- 				package_pending = "➜",
+-- 				package_uninstalled = "✗",
+-- 			},
+-- 		},
+-- 	},
+-- }
