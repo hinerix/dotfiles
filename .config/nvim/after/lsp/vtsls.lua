4362c246dfa0807ffc5b05c3ad0dd9e3495c19a9
hinericubix <197036486+hinericubix@users.noreply.github.com>
2025-05-08 17:34:58 +0900

    denoとvtslsの起動を切り替える設定を追加

--- .config/nvim/after/lsp/vtsls.lua
+++ .config/nvim/after/lsp/vtsls.lua
@@ -1,5 +1,17 @@
+---@type vim.lsp.Config
 return {
-	single_file_support = false,
+	cmd = { 'vtsls', '--stdio' },
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'javascript.jsx',
+    'typescript',
+    'typescriptreact',
+    'typescript.tsx',
+  },
+	root_markers = {
+		"package.json",
+	},
 	workspace_required = true,
 	setting = {
 		typescript = {
