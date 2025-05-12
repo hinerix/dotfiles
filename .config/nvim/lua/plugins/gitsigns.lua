55ac1a706e1a0c231b16e65d1726bef19414553a
hinerix <197036486+hinerix@users.noreply.github.com>
2025-05-13 02:05:34 +0900

    visual modeのキーマップを追加

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,5 +5,14 @@ return {
 		local utils = require("utils")
 		local keymapOpts = utils.keymapOpts
 		vim.keymap.set("n", "<Leader>hs", gitsigns.stage_hunk, keymapOpts("Stage/unstage hunks"))
+		vim.keymap.set("n", "<Leader>hr", gitsigns.reset_hunk, keymapOpts("Reset hunks"))
+
+		vim.keymap.set("v", "<Leader>hs", function()
+			gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end, keymapOpts("Stage/unstage hunks"))
+
+		vim.keymap.set("v", "<Leader>hr", function()
+			gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end, keymapOpts("Reset hunks"))
 	end
 }
