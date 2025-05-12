fbd75387c579142763ba3bfb587d192736c2b2e3
hinerix <197036486+hinerix@users.noreply.github.com>
2025-05-13 01:21:08 +0900

    hunkをstage/unstageするキーマップを追加

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,9 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	opts = {},
+	config = function()
+		local gitsigns = require("gitsigns")
+		local utils = require("utils")
+		local keymapOpts = utils.keymapOpts
+		vim.keymap.set("n", "<Leader>hs", gitsigns.stage_hunk, keymapOpts("Stage/unstage hunks"))
+	end
 }
