542d5b45292a19f9e776781c4b2c8e7f9654af1f
hinerix <197036486+hinerix@users.noreply.github.com>
2025-06-30 22:13:57 +0900

    pnpmを追加、nodeのバージョンを24に変更

--- fish/config.fish
+++ fish/config.fish
@@ -4,11 +4,4 @@ if status is-interactive
 	#activate mise
 	~/.local/bin/mise activate fish | source
 
-	# pnpm
-	set -gx PNPM_HOME "/home/hinerix/.local/share/pnpm"
-	if not string match -q -- $PNPM_HOME $PATH
-	  set -gx PATH "$PNPM_HOME" $PATH
-	end
-	# pnpm end
-
 end
