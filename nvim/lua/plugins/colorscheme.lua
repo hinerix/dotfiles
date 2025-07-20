a792b89cce05385b5de83e2e508a4fcfa3f65840
hinerix <197036486+hinerix@users.noreply.github.com>
2025-07-20 23:39:35 +0900

    add color sheme setting for which_key

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,12 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+		local catppuccin = require("catppuccin")
+		catppuccin.setup({
+			integrations = {
+				which_key = true,
+			},
+		})
 		vim.cmd([[colorscheme catppuccin-mocha]])
 	end,
 }
