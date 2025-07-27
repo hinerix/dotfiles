ec7a18726e96f49233d5820afc979a0237f2bc15
hinerix <197036486+hinerix@users.noreply.github.com>
2025-07-27 11:23:38 +0900

    add oil.nvim

---
+++ nvim/lua/plugins/oil/init.lua
@@ -0,0 +1,60 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+	dependencies = {
+		"refractalize/oil-git-status.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+  lazy = false,
+	cmd = { "Oil" },
+	keys = {
+		{
+			"<leader>e",
+			function()
+				vim.cmd.Oil()
+			end,
+		},
+	},
+	opts = function()
+		local custom_actions = require("plugins.oil.actions")
+		---@type oil.setupOpts
+		return {
+			keymaps = {
+				["?"] = "actions.show_help",
+				["gx"] = "actions.open_external",
+				["<CR>"] = "actions.select",
+				["-"] = "actions.parent",
+				["<C-p>"] = "actions.preview",
+				["gp"] = custom_actions.weztermPreview,
+				["<esc>"] = "actions.close",
+				["q"] = nil,
+				["<C-l>"] = "actions.refresh",
+				["_"] = "actions.open_cwd",
+				["`"] = "actions.cd",
+				["~"] = "actions.tcd",
+				["g."] = "actions.toggle_hidden",
+				["<C-s>"] = "actions.select_vsplit",
+				["<C-h>"] = "actions.select_split",
+				["<C-t>"] = "actions.select_tab",
+			},
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name, _)
+					local ignore_list = { ".DS_Store" }
+					return vim.tbl_contains(ignore_list, name)
+				end,
+			},
+			use_default_keymaps = false,
+			delete_to_trash = true,
+			experimental_watch_for_changes = false,
+			win_options = {
+				signcolumn = "yes:2",
+			},
+		}
+	end,
+	config = function(_, opts)
+		require("oil").setup(opts)
+		require("oil-git-status").setup()
+	end,
+}
