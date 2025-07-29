a22e8043c891dfd332ad3c4fc8b55d1d6bc22a85
hinerix <197036486+hinerix@users.noreply.github.com>
2025-07-30 01:03:26 +0900

    add installing parser logic

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
 	branch = "main",
-	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter").install({
+		local nvim_treesitter = require("nvim-treesitter")
+		local languages = {
 			"lua",
 			"markdown",
 			"typescript",
@@ -16,6 +15,35 @@ return {
 			"json",
 			"dockerfile",
 			"yaml",
+		}
+
+		local function list_to_set(list)
+			local set = {}
+			for _, item in ipairs(list) do
+				set[item] = true
+			end
+			return set
+		end
+
+		local installed_parsers = list_to_set(nvim_treesitter.get_installed("parsers"))
+		local parsers_to_install = {}
+		for _, lang in ipairs(languages) do
+			if not installed_parsers[lang] then
+				table.insert(parsers_to_install, lang)
+			end
+		end
+
+		if #parsers_to_install > 0 then
+			nvim_treesitter.install(parsers_to_install)
+		end
+
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = languages,
+			callback = function()
+				vim.treesitter.start()
+				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
 	end
 }
