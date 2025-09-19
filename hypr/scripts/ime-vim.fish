c498db0e352fa8933fd385bd32b84c763ea8f6b4
hinerix <197036486+hinerix@users.noreply.github.com>
2025-09-19 21:06:00 +0900

    動くように設定

--- hypr/scripts/ime-vim.fish
+++ hypr/scripts/ime-vim.fish
@@ -1,16 +1,19 @@
 #!/usr/bin/fish
-set FloatingVim (hyprctl clients -j | jq -c '.[] | select(.class == "FloatingVim" and .floating == true)')
+set FloatingVim (hyprctl clients -j | jq -c '.[] | select(.class == "FloatingVim")')
 if test -z "$FloatingVim"
-	alacritty \
-		--class FloatingVim \
-		--dimensions 55 18 \
-		-o window.opacity=0.5
-		-e vim -c ":IM" /var/tmp/ime-vim
+    alacritty \
+        --class FloatingVim \
+        -o 'window.dimensions.columns=55' \
+        -o 'window.dimensions.lines=18' \
+        -o 'window.opacity=0.5' \
+        -e nvim -c ":IM" /var/tmp/ime-vim
 else
-	if test (echo $FloatingVim | jq .focused) = true
-		wtype -k escape 0y$
-		hyprctl dispatch movetoworkspace special
-	else
-		hyprctl dispatch focuswindow "class:^(FloatingVim)$"
-	end
+    if test (hyprctl activewindow -j | jq -r .class) = "FloatingVim"
+        # もしウィンドウがフォーカスされている場合
+        hyprctl dispatch movetoworkspace special
+				hyprctl dispatch togglespecialworkspace
+    else
+        # もしウィンドウがフォーカスされていない場合
+        hyprctl dispatch focuswindow 'class:^(FloatingVim)$'
+    end
 end
