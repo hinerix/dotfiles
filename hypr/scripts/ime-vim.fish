53532e348c72d090f2f55b8ffa808df73f13e9c8
hinerix <197036486+hinerix@users.noreply.github.com>
2025-09-20 19:56:47 +0900

    change class name FloatingVim to VIME

--- hypr/scripts/ime-vim.fish
+++ hypr/scripts/ime-vim.fish
@@ -1,19 +1,19 @@
 #!/usr/bin/fish
-set FloatingVim (hyprctl clients -j | jq -c '.[] | select(.class == "FloatingVim")')
-if test -z "$FloatingVim"
+set VIME (hyprctl clients -j | jq -c '.[] | select(.class == "VIME")')
+if test -z "$VIME"
     alacritty \
-        --class FloatingVim \
+        --class VIME \
         -o 'window.dimensions.columns=55' \
         -o 'window.dimensions.lines=18' \
         -o 'window.opacity=0.5' \
         -e nvim -c ":IM" /var/tmp/ime-vim
 else
-    if test (hyprctl activewindow -j | jq -r .class) = "FloatingVim"
+    if test (hyprctl activewindow -j | jq -r .class) = "VIME"
         # もしウィンドウがフォーカスされている場合
-        hyprctl dispatch movetoworkspace special
+				hyprctl dispatch movetoworkspace special
 				hyprctl dispatch togglespecialworkspace
     else
         # もしウィンドウがフォーカスされていない場合
-        hyprctl dispatch focuswindow 'class:^(FloatingVim)$'
+        hyprctl dispatch focuswindow 'class:^(VIME)$'
     end
 end
