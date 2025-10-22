6492cfab0c4492e49e73e6cd384368032d0f4689
hinerix <197036486+hinerix@users.noreply.github.com>
2025-10-22 23:44:02 +0900

    add severity_sort

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -5,6 +5,8 @@ return {
 		vim.api.nvim_create_user_command("LspHealth", "checkhealth vim.lsp", { desc = "LSP health check" })
 
 		vim.diagnostic.config({
+			-- https://zenn.dev/vim_jp/articles/c62b397647e3c9
+			severity_sort = true,
 			virtual_text = true,
 			float = {
 				source = true,
