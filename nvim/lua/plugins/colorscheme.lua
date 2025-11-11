899ed3c4afaf99d4278c96f65cc7d2d3ebc65d75
hinerix <197036486+hinerix@users.noreply.github.com>
2025-11-11 23:52:24 +0900

    Pmenuの調整

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -9,12 +9,43 @@ return {
 				which_key = true,
 			},
 			transparent_background = true,
-			float = {
-				transparent = false,
-			},
 			no_italic = true,
 			no_underline = true,
+			custom_highlights = function(colors)
+				return {
+					Pmenu = { bg = colors.surface0 },
+					PmenuSel = { bg = colors.green, fg = colors.base },
+					PmenuMatchSel = { fg = colors.surface2 },
+				}
+			end
 		})
 		vim.cmd([[colorscheme catppuccin-mocha]])
 	end,
 }
+
+-- 	rosewater = "#f5e0dc",
+-- 	flamingo = "#f2cdcd",
+-- 	pink = "#f5c2e7",
+-- 	mauve = "#cba6f7",
+-- 	red = "#f38ba8",
+-- 	maroon = "#eba0ac",
+-- 	peach = "#fab387",
+-- 	yellow = "#f9e2af",
+-- 	green = "#a6e3a1",
+-- 	teal = "#94e2d5",
+-- 	sky = "#89dceb",
+-- 	sapphire = "#74c7ec",
+-- 	blue = "#89b4fa",
+-- 	lavender = "#b4befe",
+-- 	text = "#cdd6f4",
+-- 	subtext1 = "#bac2de",
+-- 	subtext0 = "#a6adc8",
+-- 	overlay2 = "#9399b2",
+-- 	overlay1 = "#7f849c",
+-- 	overlay0 = "#6c7086",
+-- 	surface2 = "#585b70",
+-- 	surface1 = "#45475a",
+-- 	surface0 = "#313244",
+-- 	base = "#1e1e2e",
+-- 	mantle = "#181825",
+-- 	crust = "#11111b",
