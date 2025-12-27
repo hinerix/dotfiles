5ad1f9958cc4fa2ad78d6694f6c830b117febe3f
hinerix <197036486+hinerix@users.noreply.github.com>
2025-12-27 12:44:16 +0900

    add Slack Shortcut

---
+++ hypr/scripts/slack.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/fish
+set -l class_name "Slack"
+set slack_client (hyprctl clients -j | jq --arg cn "$class_name" -c '.[] | select(.class == $cn)')
+
+if test -z $slack_client
+		slack
+else
+    if string match -q -- $class_name (hyprctl activewindow -j | jq -r .class)
+        hyprctl dispatch togglespecialworkspace $class_name
+    else
+        hyprctl dispatch focuswindow "class:^($class_name)\$"
+    end
+end
