afb4c36329678bc6fc8ba80f8673d4bd6440ceba
hinerix <197036486+hinerix@users.noreply.github.com>
2026-01-30 22:43:16 +0900

    fix typo

--- nvim/lua/plugins/sqls.lua
+++ nvim/lua/plugins/sqls.lua
@@ -3,8 +3,8 @@ return {
 	config = function()
 		local keymapOpts = require("utils").keymapOpts
 		vim.keymap.set({ "n", "v" }, "<Leader>sr", "<Cmd>SqlsExecuteQuery<CR>", keymapOpts("Run SQL on the buffer(or selected)"))
-		vim.keymap.set("n", "<Leader>sc", "<Cmd>SqlsConnectionChoice<CR>", keymapOpts("Choice a connection"))
-		vim.keymap.set("n", "<Leader>sd", "<Cmd>SqlsDatabaseChoice<CR>", keymapOpts("Choice a database"))
+		vim.keymap.set("n", "<Leader>sc", "<Cmd>SqlsSwitchConnection<CR>", keymapOpts("Choice a connection"))
+		vim.keymap.set("n", "<Leader>sd", "<Cmd>SqlsSwitchDatabase<CR>", keymapOpts("Choice a database"))
 		vim.keymap.set("n", "<Leader>st", "<Cmd>SqlsShowTables<CR>", keymapOpts("Show connectioins on current connection"))
 	end,
 }
