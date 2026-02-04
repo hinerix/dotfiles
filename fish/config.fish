5ca4cc5dc9a714a075776e091c473358b4368134
hinerix <197036486+hinerix@users.noreply.github.com>
2026-02-04 15:43:28 +0900

    add mise activate settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,9 @@
 if status is-interactive
     set -g theme_display_git_default_branch yes
 
-	#activate mise
-	#~/.local/bin/mise activate fish | source
+	# activate mise
+	/usr/bin/mise activate fish | source
+
   # theme
   fish_config theme choose catppuccin-mocha
 
