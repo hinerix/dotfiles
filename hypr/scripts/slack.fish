f90ada677ead9db590ef3f1eb70323b98d83ceab
hinerix <197036486+hinerix@users.noreply.github.com>
2026-02-22 01:21:35 +0900

    chore(hypr): 特定アプリケーションを開いている状態のキーバインドの挙動を調整

--- hypr/scripts/slack.fish
+++ hypr/scripts/slack.fish
@@ -3,10 +3,10 @@ set -l class_name "Slack"
 set slack_client (hyprctl clients -j | jq --arg cn "$class_name" -c '.[] | select(.class == $cn)')
 
 if test -z $slack_client
-		slack
+    slack
 else
     if string match -q -- $class_name (hyprctl activewindow -j | jq -r .class)
-				hyprctl dispatch focuscurrentorlast
+        hyprctl dispatch focusworkspaceoncurrentmonitor +1
     else
         hyprctl dispatch focuswindow "class:^($class_name)\$"
     end
--- hypr/scripts/spotify.fish
+++ hypr/scripts/spotify.fish
@@ -3,10 +3,10 @@ set -l class_name "spotify"
 set spotify_client (hyprctl clients -j | jq --arg cn "$class_name" -c '.[] | select(.class == $cn)')
 
 if test -z $spotify_client
-		spotify
+    spotify
 else
     if string match -q -- $class_name (hyprctl activewindow -j | jq -r .class)
-				hyprctl dispatch focuscurrentorlast
+        hyprctl dispatch focusworkspaceoncurrentmonitor +1
     else
         hyprctl dispatch focuswindow "class:^($class_name)\$"
     end
